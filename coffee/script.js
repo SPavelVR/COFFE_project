2fa0de4c864d4e34d4d85f35462c5b549c0ad797
SPavelVR <118850733+SPavelVR@users.noreply.github.com>
2022-11-29 17:43:10 +0200

    Add files via upload

--- coffee/script.js
+++ coffee/script.js
@@ -1,10 +1,17 @@
-function imgSing(anything) {
+function imgSlider(anything) {
     document.querySelector(".starbucks").src = anything
+}
+
+function changeCircleColor(number) {
+    const cicrle = document.querySelector('.circle')
+    let color = ['#017143', '#eb7495', '#d752b1']
+    cicrle.style.background = color[number - 1]
 }
 
 const imgs = document.querySelectorAll(".imgBolder")
 imgs.forEach((item) => {
     item.onclick = function() {
-        imgSing(`img/img${item.src.at(-5)}.png`)
+        imgSlider(`img/img${item.src.at(-5)}.png`)
+        changeCircleColor(item.src.at(-5))
     }
 })
